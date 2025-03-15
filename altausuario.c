733ed16e152e50a3a547f58e8697f51b92e02c25
AlezPaz48 <agpaz90@gmail.com>
2025-03-14 19:27:57 -0500

    alta paso 01

---
+++ altausuario.c
@@ -0,0 +1,32 @@
+/* ⁠Programa para alta de usuario
+	Descripción: Escribir un programa en C que solicite al usuario su nombre, edad y altura, y luego muestre 
+    esa información en pantalla.
+	Ejemplo de ejecución:
+        - Ingrese su nombre: Juan Pérez  
+        -  Ingrese su edad: 25  
+        - Ingrese su altura en metros: 1.75  
+
+	Salida: 
+		Hola Juan Pérez, tienes 25 años y mides 1.75 metros
+*/
+
+
+#include <stdio.h>
+
+int main () {
+
+char nombre[20];
+int edad;
+float altura;
+
+    printf("Ingrese su nombre:");
+scanf("%s",nombre);
+
+
+
+
+
+    
+    
+    
+    return 0;}
